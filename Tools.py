c798eda0a9d2e8d865c419b0d1cc52dfdbaf744f
keshavgoyal21 <keshavgoyal006@gmail.com>
2026-01-23 10:49:46 +0530

    data to xml conversion

---
+++ Tools.py
@@ -0,0 +1,25 @@
+# we have to enter the target as ip or url 
+
+
+
+
+def scan(){
+    c1  = "command": "wapiti -u {target} -f txt -o wapiti_report.txt",
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+def main(){
+    pass
+}
